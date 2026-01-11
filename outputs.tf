be6e4134b7a0822258fe6073dbe570ea5138286f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:23:50 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "spring_cloud_application_live_views" {
+  description = "All spring_cloud_application_live_view resources"
+  value       = azurerm_spring_cloud_application_live_view.spring_cloud_application_live_views
+}
+

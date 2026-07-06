9f6cdedfbc8588ceaacad81ce8e837b41211448e
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:15:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "spring_cloud_application_live_views" {
   description = "All spring_cloud_application_live_view resources"
   value       = azurerm_spring_cloud_application_live_view.spring_cloud_application_live_views
 }
+output "spring_cloud_application_live_views_name" {
+  description = "List of name values across all spring_cloud_application_live_views"
+  value       = [for k, v in azurerm_spring_cloud_application_live_view.spring_cloud_application_live_views : v.name]
+}
+output "spring_cloud_application_live_views_spring_cloud_service_id" {
+  description = "List of spring_cloud_service_id values across all spring_cloud_application_live_views"
+  value       = [for k, v in azurerm_spring_cloud_application_live_view.spring_cloud_application_live_views : v.spring_cloud_service_id]
+}
 
